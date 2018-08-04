a8afe03c78110853c589165e58e0c2252822327c
jimmywei01 <min1990719@gmail.com>
2018-08-04 20:08:41 +0800

    Update 2018-08-04T12:08:36.560Z

---
+++ js/all.js
@@ -0,0 +1,2 @@
+"use strict";$(".block1").on("click",function(){$(".block1").addClass("changeBorder")}),$(".block1").on("click",function(){$(".arrow1").addClass("arrowColor")}),$(".block2").on("click",function(){$(".block2").addClass("changeBorder")});
+//# sourceMappingURL=all.js.map
